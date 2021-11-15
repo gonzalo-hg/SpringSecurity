9e666a033469aec4d2d836df1492d29de57b1b23
gonzalohg <gonzalo.hg19@gmail.com>
2021-11-14 22:42:39 -0600

    Creacion de usuarios

--- src/main/java/com/uam/aga/app/SpringSecurityDosApplication.java
+++ src/main/java/com/uam/aga/app/SpringSecurityDosApplication.java
@@ -1,10 +1,15 @@
 package com.uam.aga.app;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.uam.aga.app.models.Rol;
+import com.uam.aga.app.models.Usuario;
 import com.uam.aga.app.services.UsuarioService;
 
 @SpringBootApplication
@@ -26,19 +31,19 @@ public class SpringSecurityDosApplication {
 	@Bean
 	CommandLineRunner run(UsuarioService usuarioService) {
 		return args ->{
-			/*usuarioService.guardarRole(new Rol("admin"));
-			usuarioService.guardarRole(new Rol("user"));
+			usuarioService.saveRole(new Rol("admin"));
+			usuarioService.saveRole(new Rol("user"));
 			
-			usuarioService.guardarUsuario(new Usuario(null, "gonzalo", "zalo1", "1234", new ArrayList<>()));
-			usuarioService.guardarUsuario(new Usuario(null, "ulises", "ulises1", "1234", new ArrayList<>()));
+			usuarioService.saveUsuario(new Usuario(null, "gonzalo", "zalo1", "1234", new ArrayList<>()));
+			usuarioService.saveUsuario(new Usuario(null, "ulises", "ulises1", "1234", new ArrayList<>()));
 
-			usuarioService.agregaRolUsuario("zalo1", "admin");
-			usuarioService.agregaRolUsuario("zalo1", "user");
-			usuarioService.agregaRolUsuario("ulises1", "user");
+			usuarioService.addRolUsuario("zalo1", "admin");
+			usuarioService.addRolUsuario("zalo1", "user");
+			usuarioService.addRolUsuario("ulises1", "user");
 			
-			usuarioService.guardarUsuario(new Usuario(null, "antonio", "anton1", "5874", new ArrayList<>()));
-			usuarioService.agregaRolUsuario("anton1", "user");*/
-			//usuarioService.agregaRolUsuario("anton1", "user");
+			usuarioService.saveUsuario(new Usuario(null, "antonio", "anton1", "5874", new ArrayList<>()));
+			usuarioService.addRolUsuario("anton1", "user");
+
 		};
 	}
 	
